296750f12bd76d934479226e9d2657adf2b501d9
Emilie Thoreson <thoresonemilie@gmail.com>
2017-06-27 08:42:11 -0700

    build skeleton of if loop

--- App.java
+++ App.java
@@ -9,6 +9,10 @@ public class App {
     CD gaga = new CD("Lady Gaga", "Joanne", 2017, 9);
     CD gwen = new CD("Gwen Stefani", "LAMB", 2006, 6);
     CD chance = new CD("Chance the Rapper", "Coloring Book", 2016, 3);
+    CD nat = new CD("Nat King Cole", "The Unforgettable", 1960, 9);
+    CD prince = new CD("Prince", "Parade", 1986, 12);
+    CD taylor1 = new CD("Taylor Swift", "1989", 2014, 10);
+    CD taylor2 = new CD("Taylor Swift", "Red", 2012, 8);
 
     CD[] inventory = {gaga, gwen, chance};
 
@@ -25,8 +29,15 @@ public class App {
         System.out.println( ep.mReleaseYear );
         System.out.println( ep.mPrice );
       }
+    } else if (navigationChoice.equals("Search by Price")) {
+        System.out.println("What is the maximum price you would pay?");
+        Integer userMaxPrice = Integer.parseInt(myConsole.readLine());
+    } else if (navigationChoice.equals("Search by Year")) {
+        System.out.println("Logic not yet built");
+    } else if (navigationChoice.equals("Search by Artist")) {
+        System.out.println("Logic not yet built");
     } else {
-      System.out.println("Not yet coded");
+        System.out.println("Logic not yet built");
     }
 
   }
